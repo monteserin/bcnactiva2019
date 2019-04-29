f5137b6f563bf32987ab706b0566ce8f31c17fd8
paramonty@gmail.com <paramonty@gmail.com>
2019-04-29 17:50:45 +0200

    amor

--- java/src/com/pablomonteserin/Ahorcado.java
+++ java/src/com/pablomonteserin/Ahorcado.java
@@ -3,15 +3,30 @@ package com.pablomonteserin;
 public class Ahorcado {
 
 	public static void main (String args[]) {
-		char [] letras = obtenerPalabraSecreta();
-		System.out.println(letras);	
+		char [] letras = getPalabraSecreta();
+		char [] guiones = getPalabraConGuiones(letras);
+		imprimirConEspaciosEnBlanco(guiones);
 	}
 	
-	static char[] obtenerPalabraSecreta() {
+	static char[] getPalabraSecreta() {
 		String [] palabras = {"amor", "odio", "indiferencia"};
 		int indice = (int) (Math.random()*palabras.length);
 		String palabraSecreta= palabras[indice];
 		char [] letrasSecretas = palabraSecreta.toCharArray();
 		return letrasSecretas;
+	}
+	
+	static char[] getPalabraConGuiones(char[]psecreta) {
+		char [] palabraConGuiones = new char[psecreta.length];
+		for(int i = 0; i< palabraConGuiones.length; i++) {
+			palabraConGuiones[i] = '_';
+		}
+		return palabraConGuiones;
+	}
+	
+	static void imprimirConEspaciosEnBlanco(char [] palabra) {
+		for(int i = 0; i<palabra.length; i++) {
+			System.out.print(palabra[i] + " ");
+		}
 	}
 }
